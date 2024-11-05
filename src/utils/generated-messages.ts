f6ff8ba9530735f0211793ec0601393ba7b5c840
Jerico Rodriguez <j.rodriguez.enz@gmail.com>
2024-11-05 18:42:25 +0100

    renamed constants

--- src/utils/generated-messages.ts
+++ src/utils/generated-messages.ts
@@ -1,15 +1,15 @@
-export const aboutMe =
+export const ABOUTME =
   "<p>Hello, I'm Jerico. 👋<br /><br />Web Developer with 2 years of professional experience, using technologies such as Java, TypeScript, Angular, SAP Hybris, and SAP Composable Storefront.<br /><br />I have a Higher Technical Degree in Web Application Development + mobile applications (Bilingual), graduated from IES EL LAGO.<br /><br />A hardworking and persevering person with great ease in working in teams and handling any problem that comes my way, allowing me to play a strong role in the position applied for.</p>";
-export const projects =
+export const PROJECTS =
   '<p>Proyectos en los que he participado:<br />- Samsung<br>- Estrella Galicia<br>- Forter<br>- Garcia Carrion</p>';
-export const linkedIn =
+export const LINKEDIN =
   '<p>you can find me on <a target="_blank" href="https://www.linkedin.com/in/jerico-rodriguez/">LinkedIn ~ Profile</a></p>';
-export const github =
+export const GITHUB =
   '<p>you can find me on <a target="_blank" href="https://github.com/jerirgz">GitHub ~ Profile</a></p>';
-export const contact =
+export const CONTACT =
   '<p>Contact me:</p><br /><p>Email: <a href="mailto:j.rodriguez.enz@gmail.com">j.rodriguez.enz@gmail.com</a></p><br /><p>Phone number: <span>+34 657143012<span></p>';
-export const cv =
+export const CV =
   '<p>Dowload my cv ~ <span><a href="../resources/curriculum.pdf" download>DOWNLOAD</a></span></p><br />';
 // GUEST CLI
-export const guestCli =
+export const GUESTCLI =
   '<div class="terminal-cli"><p class="bash-user">welcome<span>@guest ></span></p></div>';
